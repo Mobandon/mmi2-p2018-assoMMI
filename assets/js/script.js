2513abe6449dd294842b93570566ee18f934bea9
Mobandon <31806656+Mobandon@users.noreply.github.com>
2018-02-09 10:47:04 +0100

    JSON interraction

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 
   
     var showData = $('#show-data');
+    var details = $('#details');
 
     $.getJSON('membres.json', function (data) {
       console.log(data);
@@ -10,16 +11,42 @@ $(document).ready(function () {
         return item.nom + ': ' + item.carac + '.tel: ' + item.contact;
       });
 
+      var listmembre = data.membres.map(function (item) {
+        return "<li class='membre' id=" + item.id + ">" + item.nom + "</li>";
+      });
+
+
       showData.empty();
 
-      if (items.length) {
+      if (listmembre.length) {
 
-        var content = '<li>' + items.join('</li><li>') + '</li>';
+        var content = listmembre.join('');
         var list = $('<ul />').html(content);
         showData.append(list);
       }
+
+      var coll = document.getElementsByClassName("membre");
+      //console.log(coll);
+      var i;
+
+      for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function(yeah) {
+          mo = yeah.target.id;
+          console.log(mo);
+
+          details.html(items[mo]);
+        });
+      }
+
+
+
+
     });
 
     showData.text('Loading the JSON file.');
+
+
+
+    
 
 });
